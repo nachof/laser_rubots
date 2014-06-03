dc5cd25663e60664fdea9634a579a8bbc3ce5fe8
Nacho Facello <nacho@nucleartesuji.com>
2014-06-03 19:16:27 -0300

    Load from filename

--- lib/rubots/strategy_loader.rb
+++ lib/rubots/strategy_loader.rb
@@ -16,10 +16,21 @@ module Rubots
     end
 
     def strategy_class
-      sample_class if is_sample?
+      if is_sample?
+        sample_class
+      else
+        class_from_file
+      end
     end
 
   private
+
+    def class_from_file
+      load @name
+
+      class_name = camelize(File.basename(@name, ".rb"))
+      Rubots.const_get(class_name)
+    end
 
     def is_sample?
       @name.match /^sample:/

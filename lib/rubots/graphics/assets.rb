cbad0d0add540e2a4069aca9bce0fab713243859
Nacho Facello <nacho@nucleartesuji.com>
2014-06-04 19:38:30 -0300

    Make sure asset routes are absolute, so we can load them when it's a gem

--- lib/rubots/graphics/assets.rb
+++ lib/rubots/graphics/assets.rb
@@ -1,10 +1,11 @@
 module Rubots
   module Graphics
     class Assets
-      PEW_SOUND = "media/pew.wav"
-      ROBOT_IMG = "media/robot.png"
-      GUN_IMG   = "media/gun.png"
-      DEAD_IMG  = "media/deadbot.png"
+      MEDIA_DIR = File.join(File.dirname(__FILE__), '..', '..', '..', 'media')
+      PEW_SOUND = File.join(MEDIA_DIR, "pew.wav")
+      ROBOT_IMG = File.join(MEDIA_DIR, "robot.png")
+      GUN_IMG   = File.join(MEDIA_DIR, "gun.png")
+      DEAD_IMG  = File.join(MEDIA_DIR, "deadbot.png")
     end
   end
 end
